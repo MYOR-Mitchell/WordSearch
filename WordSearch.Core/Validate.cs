c727867f1f3b28d304aa01f3db493a312aa291ad
MYOR-Mitchell <Martin.Mitchell@MyYahoo.com>
2025-03-10 20:24:52 -0400

    Initial commit

---
+++ WordSearch.Core/Validate.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace WordSearch.Core
+{
+    public class Validate
+    {
+        private readonly WordListState _wordListState;
+        public Validate(WordListState wordListState)
+        {
+            _wordListState = wordListState;
+        }
+
+        public void ValidWord(string word)
+        {
+            if(string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentException("Word cannot be null or empty.");
+            }
+            else if(word.Length < 3 || word.Length > 6)
+            {
+                throw new ArgumentException("Word must be between 3 and 6 characters long.");
+            }
+            else
+            {
+                _wordListState.AddWord(word.ToCharArray());
+            }
+        }
+    }
+}
---
+++ WordSearch.Core/WordListState.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace WordSearch.Core
+{
+    public class WordListState
+    {
+        private List<List<char>> _words = new List<List<char>>();
+
+        public List<List<char>> Words => _words;
+
+
+        public void AddWord(char[] word) => _words.Add(word.ToList());
+
+        public void ClearWords() => _words.Clear();
+    }
+}
